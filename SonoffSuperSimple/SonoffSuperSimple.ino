0968e188302e6b7afce301a191ff959f9f98efe5
Ola A <ola.j.andersson@gmail.com>
2019-02-11 00:09:46 +0100

    Add password and require password
    To set value, require password and POST method.
    Build against newer ESPWebConfig.

--- SonoffSuperSimple/SonoffSuperSimple.ino
+++ SonoffSuperSimple/SonoffSuperSimple.ino
@@ -3,30 +3,43 @@
  *
  * Sketch to use for sonoff "original" relay module
  * - Button press turns light on or off
- * - Long press on button clear config
+ * - Long press on button clear wifi and security config
+ *
+ * First boot/after longpress, device is in config mode.
+ * Connect a phone/laptop to access point ESP_192.168.X.Y and browse
+ * to ESP_192.168.X.Y and fill in wifi info and optionally user and
+ * password. (Not sure how complicated/long the password can be)
+ *
+ * After config the sonof listens to thgese urls:
  * - path: http://ip/switch get the value
- *         http://ip/switch?set=1 turn on power
- *         http://ip/switch?set=0 turn off power
- *         http://ip/switch?set=t toggle power
+ *         http://ip/switch?set=1 (POST) turn on power
+ *         http://ip/switch?set=0 (POST) turn off power
+ *         http://ip/switch?set=t (POST) toggle power
  */
 
+#include "Arduino.h"
 #include <ESPWebConfig.h> // https://github.com/mnomn/ESPWebConfig
 
 /*
 Sonoff WIFI Smart Switch:
-1 M flash, 64 K SPIFFS
+Arduino serttings: Generic esp8266, 1 M flash, 64 K SPIFFS
 
-Reley: GPIO 12
+Relay: GPIO 12
 Button: GPIO 0
 Led: GPIO 13
 */
 ESP8266WebServer server(80);
 int buttonPin = 0;
-ESPWebConfig espConfig("configpass");
+
+char *user = NULL;
+char *pass = NULL;
+const char* USER_KEY = "User:";
+const char* PASS_KEY = "Pass:";
+String parameters[] = {USER_KEY, PASS_KEY};
+ESPWebConfig espConfig("configpass", parameters, 2);
+
 const int relayPin = 12;
 unsigned long lastButtonTime = 0;
-int mode = 0;
-static int myTest = 0;
 
 void handleRoot() {
   server.send(200, "text/html", F("<html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
@@ -38,31 +51,41 @@ void handleRoot() {
   "You can also switch the relay with the http interface. "
   "Use the links below as bookmarks or in an app.</h3>"
   "<p>Get value: <a href=/switch>/switch</a>"
-  "<p>Turn power on: <a href=/switch?set=1>/switch?set=1</a>"
-  "<p>Turn power off: <a href=/switch?set=0>/switch?set=0</a>"
-  "<p>Toggle power: <a href=/switch?set=t>/switch?set=t</a>"
+  "<p>Turn power on: /switch?set=1 (POST)"
+  "<p>Turn power off: /switch?set=0 (POST)"
+  "<p>Toggle power: /switch?set=t (POST)"
   "<h3>Add argument \"json=1\" to get the reply in json format</h3>"
   "<p>Get value: <a href=/switch?json=1>/switch?json=1</a>"
-  "<p>Toggle power: <a href=/switch?set=t&json=1>/switch?set=t&json=1</a>"
+  "<p>Toggle power: /switch?set=t&json=1 (POST)"
   "</body></html>"));
 }
 
 void handleSwitch() {
   String out;
-  bool invalidArg = 0;
+  bool err = 0;
+  int code = 200;
 
   String set = server.arg("set");
   Serial.print("setArgument: ");
   Serial.println(set?set:"NULL");
   if (set && set.length() > 0) {
-    if (set == "0") {
+    if (pass && *pass && !server.authenticate(user, pass)) {
+      return server.requestAuthentication();
+    } else {
+      Serial.println("no pass");
+    }
+    if (server.method() == HTTP_GET) {
+      err = 1;
+      code = 405; // Method not allowed
+    } else if (set == "0") {
       digitalWrite(relayPin, LOW);
     } else if (set == "1") {
       digitalWrite(relayPin, HIGH);
     } else if (set == "t") {
       digitalWrite(relayPin, !digitalRead(relayPin));
     } else {
-      invalidArg = 1;
+      err = 1;
+      code = 400; // Generic error
     }
     // Takes some time for pin to flip
     delay(10);
@@ -73,14 +96,14 @@ void handleSwitch() {
   if (json && json == "1") {
     out = "{\"switch\":";
     out += val;
-    if (invalidArg) {
-      out += ",\"error\":\"Argument set can only have values 0, 1 or t\"";
+    if (err) {
+      out += ",\"error\":\"set must be 0, 1 or t and use POST\"";
     }
     out += "}";
-    server.send(200, "application/json", out);
+    server.send(code, "application/json", out);
   } else {
     String out = String(val, DEC);
-    server.send(200, "text/plain", out);    
+    server.send(code, "text/plain", out);
   }
 }
 
@@ -92,15 +115,19 @@ void setup() {
   // espWebConfig is configuring the button as input
   pinMode(relayPin, OUTPUT);
   Serial.println("Starting ...");
-  if (espConfig.setup(server)) {
-    Serial.println("Normal boot");
-    Serial.println(WiFi.localIP());
-    server.on ("/", handleRoot);
-    server.on ("/switch", handleSwitch);
-  } else {
-    Serial.println("Config mode");
-    mode = 1;
+  if (!espConfig.setup()) {
+    Serial.println("Config failed");
   }
+
+  user = espConfig.getParameter(USER_KEY);
+  pass = espConfig.getParameter(PASS_KEY);
+
+  Serial.println("Normal boot");
+  Serial.println(WiFi.localIP());
+  server.on ("/", handleRoot);
+  server.on ("/switch", handleSwitch);
+
+
   server.begin();
   /* Perhaps the internal pull up is not needed/bad?.
      Since there probbaly is an external pull up. */
@@ -112,9 +139,16 @@ void loop() {
   static long resetCounter = 5*1000;
   if (LOW == digitalRead(buttonPin))
   {
-    resetCounter -= 100;
+    if (resetCounter == (5*1000 - 100)) {// Toggle after 100 ms to avoid bounce
+      Serial.println("Pressed, toggle");
+      Serial.println(WiFi.localIP());
+      digitalWrite(relayPin, !digitalRead(relayPin));
+    }
+
     delay(100);
-    if (resetCounter == 0) {
+    resetCounter -= 100;
+    if (resetCounter <= 0) {
+      Serial.println("Clear config");
       espConfig.clearConfig();
       ESP.restart();
     }
@@ -125,12 +159,4 @@ void loop() {
 
   server.handleClient();
 
-  if (LOW == digitalRead(buttonPin)) {
-    unsigned long now = millis();
-    if (now - lastButtonTime > 1000) {
-      lastButtonTime = millis();
-      Serial.println("Pressed, toggle.");
-      digitalWrite(relayPin, !digitalRead(relayPin));
-    }
-  }
 }
---
+++ SonoffSuperSimple/test.sh
@@ -0,0 +1,48 @@
+#/bin/bash
+
+#Test script tested on linux. Should wark on mac too.
+
+USERPASS='-u ADMIN:PASS'
+
+echo "Test SSS. Arg 1 shall be ip nr"
+
+echo "USERPASS: $USERPASS"
+
+[ "$1" ] ||{
+    echo "No IP in arg 1"
+    exit 1
+}
+
+echo "Test $1"
+
+
+# Get HELP
+echo
+curl $1
+echo
+echo "RESULT ROOT: $?"
+
+sleep 1
+echo
+echo "#####"
+echo "GET"
+curl $1/switch
+echo
+
+sleep 1
+echo "#####"
+echo "Bad arg (expect 40x)"
+curl $USERPASS -X POST -w ", code=%{http_code}" $1'/switch?set=r'
+echo
+
+sleep 1
+echo "#####"
+echo "Good arg"
+curl $USERPASS -X POST -w ", code=%{http_code}"  $1'/switch?set=t'
+echo
+
+sleep 1
+echo "#####"
+echo "Get again, expect changed"
+curl $1/switch
+echo
